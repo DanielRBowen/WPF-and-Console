0fcd0047c175e31320d7b28e11185ad6b7dbb7c6
Daniel Bowen <danielrbowen@live.com>
2018-05-15 20:35:03 -0600

    Added Tag Folder Name To Album project

---
+++ TagFolderNameToAlbum/TagFolderNameToAlbum/InputOutput.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagFolderNameToAlbum
+{
+    class InputOutput
+    {
+        private static string FolderName { get; set; }
+
+        internal static Task ChangeAlbumTagsOfFolderAsync(string folderPath)
+        {
+            var positionOfLastBackslash = folderPath.LastIndexOf($"\\") + 1;
+            FolderName = folderPath.Substring(positionOfLastBackslash, folderPath.Length - positionOfLastBackslash);
+
+            return Task.Run(() => ChangeAlbumTagsOfFolder(folderPath));
+        }
+
+        private static void ChangeAlbumTagsOfFolder(string folderPath)
+        {
+            var mp3Files = Directory.GetFiles(folderPath, "*.mp3")
+                                     .Select(Path.GetFullPath)
+                                     .ToList();
+
+
+            mp3Files.ForEach(ChangeAlbumOfFile);
+        }
+
+        private static void ChangeAlbumOfFile(string filePath)
+        {
+            
+            TagLib.File tagFile = TagLib.File.Create(filePath);
+            tagFile.Tag.Album = FolderName;
+            tagFile.Save();
+        }
+    }
+}
---
+++ TagFolderNameToAlbum/TagFolderNameToAlbum/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace TagFolderNameToAlbum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeAlbumTagsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPath = FolderPathTextBox.Text;
+            InputOutput.ChangeAlbumTagsOfFolderAsync(folderPath);
+        }
+    }
+}
